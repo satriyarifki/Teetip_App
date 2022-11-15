673b40fb914e08f31d6375c697230eef474e7bb8
satriyarp <rifkisatriya@gmail.com>
2022-11-15 22:25:05 +0700

    Login Auth

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+import 'screens/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CheckAuth(),
+    );
+  }
+}
+
+class CheckAuth extends StatefulWidget{
+  @override
+  _CheckAuthState createState() => _CheckAuthState();
+}
+
+class _CheckAuthState extends State<CheckAuth>{
+  bool isAuth = false;
+
+  @override
+  void initState(){
+    super.initState();
+    _checkIfLoggedIn();
+  }
+
+  void _checkIfLoggedIn() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var token = localStorage.getString('token');
+    if(token != null){
+      if(mounted){
+        setState(() {
+          isAuth = true;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    Widget child;
+    if(isAuth){
+      child = Home();
+    } else{
+      child = Login();
+    }
+
+    return Scaffold(
+      body: child,
+    );
+  }
+}
